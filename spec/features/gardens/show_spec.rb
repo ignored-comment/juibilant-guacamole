c2113bb934da0524f10144b572e26f6817f837ff
ignored-comment <info.konham@gmail.com>
2021-04-27 12:45:04 -0400

    Create feature assetions for garden show page

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -5,15 +5,31 @@ RSpec.describe "Garden's Show Page" do
     @turing_garden = Garden.create!(name: "Turing Community Garden", organic: true)
 
     @turing_plot1 = @turing_garden.plots.create!(number: 25, size: "Large", direction: "East")
-    @turing_cucumber = @turing_plot1.plants.create!(name: "Pickling Cucumber", description: "Potential to be nice and salty", days_to_harvest: 100)
-
     @turing_plot2 = @turing_garden.plots.create!(number: 15, size: "Medium", direction: "South")
-    @turing_tomato = @turing_plot2.plants.create!(name: "Heirloom Tomato", description: "Tom-'ae'-toes, Tom-'ah'-toes", days_to_harvest: 35)
+
+    @cucumber = Plant.create!(name: "Pickling Cucumber", description: "Potential to be nice and salty", days_to_harvest: 100)
+    @tomato1 = Plant.create!(name: "Heirloom Tomato", description: "Tom-'ae'-toes, Tom-'ah'-toes", days_to_harvest: 35)
+    @tomato2 = Plant.create!(name: "Heirloom Tomato", description: "Tom-'ae'-toes, Tom-'ah'-toes", days_to_harvest: 35)
+    @squash = Plant.create!(name: "Squishy Squash", description: "FER THANKSGIBERN", days_to_harvest: 15)
+    @peanuts = Plant.create!(name: "G.W Carver's Homebrew Peanuts", description: "So many uses for these", days_to_harvest: 43)
+
+    Harvest.create!(plant: @cucumber, plot: @turing_plot1)
+    Harvest.create!(plant: @tomato1, plot: @turing_plot2)
+    Harvest.create!(plant: @tomato2, plot: @turing_plot1)
+    Harvest.create!(plant: @squash, plot: @turing_plot2)
+    Harvest.create!(plant: @peanuts, plot: @turing_plot1)
   end
 
   describe "as a visitor, when I visit a garden's show page" do
     it "shows me the garden's name and organic attributes, as well as a list of all the plants in that garden's plots" do
-      
+      expect(page).to have_content("Turing Community Garden")
+      expect(page).to have_content("Organic: true")
+      expect(page).to have_content("Heirloom Tomato")
+      expect(page).to have_content("Squishy Squash")
+      expect(page).to have_content("Heirloom Tomato")
+      expect(page).to have_content("G.W Carver's Homebrew Peanuts")
+
+      expect(page).not_to have_content("Pickling Cucumber")
     end
   end
 end
