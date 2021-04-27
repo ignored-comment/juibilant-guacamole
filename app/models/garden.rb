75ac5fe90de658fc9c63f57390e873716e2e2d4e
ignored-comment <info.konham@gmail.com>
2021-04-27 12:41:14 -0400

    Pass model assertions for garden (unique_harvest_under_100_days)

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,4 +1,8 @@
 class Garden < ApplicationRecord
   has_many :plots
   has_many :plants, through: :plots
+
+  def unique_harvest_under_100_days
+    plants.distinct.where('days_to_harvest < ?', 100)
+  end
 end
--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -13,22 +13,22 @@ RSpec.describe Garden do
     @turing_plot2 = @turing_garden.plots.create!(number: 15, size: "Medium", direction: "South")
 
     @cucumber = Plant.create!(name: "Pickling Cucumber", description: "Potential to be nice and salty", days_to_harvest: 100)
-    @tomato = Plant.create!(name: "Heirloom Tomato", description: "Tom-'ae'-toes, Tom-'ah'-toes", days_to_harvest: 35)
+    @tomato1 = Plant.create!(name: "Heirloom Tomato", description: "Tom-'ae'-toes, Tom-'ah'-toes", days_to_harvest: 35)
+    @tomato2 = Plant.create!(name: "Heirloom Tomato", description: "Tom-'ae'-toes, Tom-'ah'-toes", days_to_harvest: 35)
     @squash = Plant.create!(name: "Squishy Squash", description: "FER THANKSGIBERN", days_to_harvest: 15)
     @peanuts = Plant.create!(name: "G.W Carver's Homebrew Peanuts", description: "So many uses for these", days_to_harvest: 43)
 
     Harvest.create!(plant: @cucumber, plot: @turing_plot1)
-    Harvest.create!(plant: @tomato, plot: @turing_plot2)
-    Harvest.create!(plant: @tomato, plot: @turing_plot1)
+    Harvest.create!(plant: @tomato1, plot: @turing_plot2)
+    Harvest.create!(plant: @tomato2, plot: @turing_plot1)
     Harvest.create!(plant: @squash, plot: @turing_plot2)
     Harvest.create!(plant: @peanuts, plot: @turing_plot1)
   end
 
   describe 'model methods' do
     it "can return a list of all plants included in that garden's plots and I see that this list is unique and only includes plants that take less than 100 days to harvest" do
-      expect(@turing_garden.unique_harvest_under_100_days).to eq([@tomato, @squash, @peanuts])
-      expect(@turing_garden.unique_harvest_under_100_days).not_to include(@tomato)
-      expect(@turing_garden.unique_harvest_under_100_days).not_to include(@cucumber)
+      expect(@turing_garden.unique_harvest_under_100_days).to eq([@tomato1, @tomato2, @squash, @peanuts])
+      expect(@turing_garden.unique_harvest_under_100_days).not_to eq(@cucumber)
     end
   end
 end
